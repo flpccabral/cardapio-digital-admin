270105d9885f98cb92e0692d1ef566cafe533255
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-07-28 23:13:00 -0300

    up

--- src/app/api/additionalItem/[additionalItemId]/route.ts
+++ src/app/api/additionalItem/[additionalItemId]/route.ts
@@ -76,7 +76,7 @@ export async function PATCH(
                         }
                     },
                     data: {
-                        productIds: {
+                        additionalItemsIds: {
                             set: additionalItemsIds
                         }
                     }
